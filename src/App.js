86bf748da5206d1276a7da9ece9271ab65ac9aa5
Ludovic Stumme <ludovic.stumme@gmail.com>
2020-11-06 23:33:00 +0100

    New Login page design

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
 import React from 'react';
 import { makeStyles } from '@material-ui/core/styles';
 import {
-  Grid, TextField, Button, InputAdornment
+  Grid, TextField, Button, InputAdornment, Card,
+  Container, Box
 } from '@material-ui/core';
 
 import { AccountCircle, LockRounded } from '@material-ui/icons';
@@ -17,6 +18,10 @@ const useStyles = makeStyles({
   },
   formGrid: {
     padding: 10,
+    minHeight: '100vh'
+  },
+  card: {
+    padding: 20
   },
   formDiv: {
     display: 'flex',
@@ -29,26 +34,21 @@ const useStyles = makeStyles({
 function App() {
   const classes = useStyles();
   return (
-    <div>
-      <Grid container className={classes.root}>
-        <Grid item xs={12} sm={6}>
-          <img src="https://images.pexels.com/photos/193349/pexels-photo-193349.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500" className={classes.imgGrid} alt="brand" />
-        </Grid>
-        <Grid container item xs={12} sm={6} alignItems="center" direction="column" justify="space-between" className={classes.formGrid}>
-          <div />
-          <div className={classes.formDiv}>
-            <Grid container justify="center">
-              <img src="https://logos-world.net/wp-content/uploads/2020/08/CBS-Logo-1951-present-700x394.jpg" width={200} alt="logo" />
-            </Grid>
-            <TextField label="Username" InputProps={{ startAdornment: <InputAdornment position="start"><AccountCircle /></InputAdornment> }} margin="normal" />
-            <TextField label="Password" InputProps={{ startAdornment: <InputAdornment position="start"><LockRounded /></InputAdornment> }} type="password" margin="normal" />
-            <div style={{ height: 20 }} />
-            <Button color="primary" variant="contained">Submit</Button>
-          </div>
-          <div />
-        </Grid>
-      </Grid>
-    </div>
+    <Grid container alignItems="center" direction="column" justify="space-between" className={classes.formGrid}>
+      <div />
+      <Card className={classes.card}>
+        <div className={classes.formDiv}>
+          <Grid container justify="center">
+            <img src="https://logos-world.net/wp-content/uploads/2020/08/CBS-Logo-1951-present-700x394.jpg" width={200} alt="logo" />
+          </Grid>
+          <TextField label="Username" InputProps={{ startAdornment: <InputAdornment position="start"><AccountCircle /></InputAdornment> }} margin="normal" />
+          <TextField label="Password" InputProps={{ startAdornment: <InputAdornment position="start"><LockRounded /></InputAdornment> }} type="password" margin="normal" />
+          <div style={{ height: 20 }} />
+          <Button color="primary" variant="contained">LOG IN</Button>
+        </div>
+      </Card>
+      <div />
+    </Grid >
   );
 }
 
